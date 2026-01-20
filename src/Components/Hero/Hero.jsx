627c7ebbf65c1d7f023ecad3999d2c3b10f5840f
13vanshu <vanshchhoda28@gmail.com>
2026-01-20 14:28:14 +0530

    hero page responsives

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,36 +5,23 @@ import Button from '../../Tools/Button'
 const Hero = () => {
   return (
     <section>
-<div className="max-w-300 mx-auto px-4 sm:px-6 md:px-10 
-flex flex-col md:flex-row items-center 
-pt-28 sm:pt-32 md:pt-24">
+      <div className="max-w-300 mx-auto px-4 sm:px-6 md:px-10 flex flex-col md:flex-row items-center pt-28 sm:pt-32 md:pt-24">
 
         {/* Hero Content */}
         <div className="w-full md:flex-1 pt-6 sm:pt-8 md:pt-0">
           {/* Badge */}
-          <span className="inline-block bg-orange-100 text-orange-500 
-    text-xs sm:text-sm md:text-base 
-    px-3 sm:px-4 md:px-5 
-    py-1 sm:py-1.5 md:py-2 
-    rounded-full">
+          <span className="inline-block bg-orange-100 text-orange-500 text-2xl sm:text-sm md:text-base px-3 sm:px-4 md:px-5 py-1 sm:py-1.5 md:py-2 rounded-full">
             Export Best Quality...
           </span>
 
           {/* Heading */}
-          <h1 className="mt-3 sm:mt-4 font-bold 
-    text-3xl leading-tight 
-    sm:text-4xl sm:leading-snug 
-    md:text-6xl md:leading-tight 
-    lg:text-7xl">
+          <h1 className="mt-3 sm:mt-4 font-bold text-4xl leading-tight sm:text-4xl sm:leading-snug md:text-6xl md:leading-tight lg:text-7xl">
             Tasty Organic <span className="text-orange-500">Fruits</span> &{" "}
             <span className="text-orange-500">Veggies</span> In Your City
           </h1>
 
           {/* Description */}
-          <p className="mt-3 sm:mt-4 mb-5 sm:mb-6 
-    max-w-md sm:max-w-lg md:max-w-2xl 
-    text-zinc-600 
-    text-sm sm:text-base md:text-lg">
+          <p className="mt-3 sm:mt-4 mb-5 sm:mb-6 max-w-md sm:max-w-lg md:max-w-2xl text-zinc-600 text-xl sm:text-base md:text-lg">
             Bred for a high content of beneficial substances. Our products are all
             fresh and healthy.
           </p>
